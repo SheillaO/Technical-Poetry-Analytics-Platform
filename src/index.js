8d1c2f2c1d0ee8b375fa93d1a04dc3ec7fb96499
SheillaO <sheilla.alusa@strathmore.edu>
2025-07-03 18:24:05 +0300

    Added prompts, apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "Technical poem based on market trends",
+function displayPoem(response) {
+  
+  console.log("poem generated")
+    new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "7601b0fff0179o9d5059a8db34ctbc66";
+  let prompt = "Write a technical poem based on current market trends.";
+  let context = "Financial markets, technology, analytics.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  
+  console.log("generating poem")
+  
+  axios.get(apiURL).then (displayPoem);
+  
+  
+  
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
